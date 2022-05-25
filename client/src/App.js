e49ddb87c6b04af94630e00bd8f3a9a295c6b931
rceglie <ceglierob@gmail.com>
2022-05-24 23:01:07 -0400

    Finished part 1 of tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import { useDispatch } from 'react-redux';
 
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import soxpicture from './images/sox.jpg'
+import soxpicture from './images/sox.jpg';
+
+import "./style/app.css"
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch]);
+
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={soxpicture} alt="" height="200" />
+            <AppBar className="" position="static" color="inherit">
+                <Typography className="" variant="h2" align="center">Memories</Typography>
+                <img className="soxlogo" src={soxpicture} alt="" height="200" />
             </AppBar>
             <Grow in>
                 <Container>
