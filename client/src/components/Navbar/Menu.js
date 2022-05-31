0f0c93589b340a5a74b37a23a604b221e45890e5
rceglie <ceglierob@gmail.com>
2022-05-30 22:01:18 -0400

    More style - finished header and started on menu popup

--- client/src/components/Navbar/Menu.js
+++ client/src/components/Navbar/Menu.js
@@ -2,45 +2,54 @@ import React, { useEffect, useState } from 'react';
 import "../../style/menu.css";
 import MenuIcon from '@mui/icons-material/Menu';
 import { Button } from '@mui/material';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 const Menu = (props) => {
 
-    const [modal, setModal] = useState(false);
+    const [modal2, setModal2] = useState(false);
 
-    const toggleModal = () => {
-        if (!modal){
+    const toggleModal2 = () => {
+        if (!modal2){
             window.scrollTo({ top: 0, behavior: 'smooth' });
         }
-        setModal(!modal)
+        setModal2(!modal2)
     };
 
     useEffect(() => {
         
     })
 
-    if(modal) {
-        document.body.classList.add('active-modal')
+    if(modal2) {
+        document.body.classList.add('active-modal2')
     } else {
-        document.body.classList.remove('active-modal')
+        document.body.classList.remove('active-modal2')
     }
     
     return (
         <>
-        <Button onClick={toggleModal} >
-            <MenuIcon sx={{ fontSize: 70 }}/>
+        <Button onClick={toggleModal2} >
+            <MenuIcon style={{color: "#0000FF"}} sx={{ fontSize: 70 }}/>
         </Button>
     
-          {modal && (
-            <div className="modal">
-              <div onClick={toggleModal} className="overlay"></div>
-              <div className="modal-content">
-                <button className="close-modal" onClick={toggleModal}> CLOSE</button>
-                <h2>Cool menu</h2>
-                <button>Create Post</button>
-                <button>My Posts</button>
-                <button>Account Info</button>
-                <button>About Byblos</button>
+          {modal2 && (
+            <div className="">
+              <div className="popup-box">
+                <div className="box">
+                  <div className="menu-header">
+                    <button onClick={toggleModal2} className="close-btn">
+                      <KeyboardArrowLeftIcon style={{color: "#0000FF"}} sx={{ fontSize: 70 }}/>
+                    </button>
+                    <p className="menu-title">Menu Stuff</p>
+                  </div>
+                  <div className="menu-buttons">
+                    <button>Create Post</button>
+                    <button>My Posts</button>
+                    <button>Account Info</button>
+                    <button>About Byblos</button>
+                  </div>
+                </div>
               </div>
+                
             </div>
           )}
           </>
